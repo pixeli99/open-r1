62e0b3ea4f3ecac150ad23e2125758f36bfb238c
pixeli99 <lipengxiang@mail.dlut.edu.cn>
2025-04-24 02:47:05 -0700

    123

--- baseline.sh
+++ baseline.sh
@@ -5,6 +5,6 @@ export HF_ENDPOINT=https://hf-mirror.com
 
 # accelerate launch --config_file recipes/accelerate_configs/zero2.yaml --num_processes 7 src/open_r1/grpo.py\
 #     --config recipes/Qwen2.5-1.5B-Instruct/grpo/config_demo.yaml
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/ddp.yaml --main_process_port 29501 \
+ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/ddp.yaml --main_process_port 29509 \
     src/open_r1/sft.py \
     --config recipes/Qwen2.5-1.5B-Instruct/sft/config_demo.yaml
--- loop.sh
+++ loop.sh
@@ -7,4 +7,4 @@ export HF_ENDPOINT=https://hf-mirror.com
 #     --config recipes/Qwen2.5-1.5B-Instruct/grpo/config_demo.yaml
 ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/ddp.yaml \
     src/open_r1/sft.py \
-    --config recipes/Qwen2.5-1.5B-Instruct/sft/config_demo.yaml
+    --config recipes/Qwen2.5-1.5B-Instruct/sft/config_demo_loop.yaml
